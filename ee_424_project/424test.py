95e1e3977647d99c230940277ee65ad0dadc68fd
Valery Smith <galval28@gmail.com>
2018-03-22 17:30:00 -0500

    Add files via upload
    code using the scipy library to get number data from.wav files in this case it uses and AJJ song

---
+++ ee_424_project/424test.py
@@ -0,0 +1,19 @@
+import wave,math,os, audioop, scipy
+from playsound import playsound
+import scipy.io 
+from scipy.io import wavfile
+
+
+print("hello world")
+#first open the wave file jk this fucntion is suposed to do both?
+
+
+#then read the wave file
+samp , Braver=wavfile.read('AJJBrave.wav')
+
+
+
+print(Braver)
+
+print(Braver[44100:44200])
+#print('\n'.join([str(pair) for pair in Braver])
