a2efb3b9a42d0bf268af10fac82a76b0141e3068
Valery Smith <galval28@gmail.com>
2018-04-22 22:23:56 -0500

    Update 424test.py
    updated so that the data is stored better

--- ee_424_project/424test.py
+++ ee_424_project/424test.py
@@ -10,13 +10,11 @@ from numpy import fft
 import plotly
 from plotly.graph_objs import Scatter, Layout #this is for plotting maybe unessecary
 import pandas as pd
-import tensorflow as tf
+import tensorflow as tfs
 #there are 4 artists with 0-19 songs each
 
-ydl_opts = {}
-artists = ["NY","TS","FM","TP"]
-trainingDic={"NY":[[],[]],"TS":[[],[]],"FM":[[],[]],"TP":[[],[]]}
-songdata=[[0,0],[0,0],[0,0],[0,0]] #initialize the array
+
+trainingDic={"NY":[],"TS":[],"FM":[],"TP":[]}
 #the input needs to be formatted as a dictionary 
 #dictionaries are cool
 #for urls in song_list:
@@ -29,41 +27,27 @@ for file in os.listdir(location):
     
     if file.endswith(".wav") and file.startswith("NY"):
         #print(file)
-        trainingDic['NY'][i]=wavfile.read(file) #put tthe sampling frequency and the song data as a row in the array
-        i=i+1
-    i=0    
+        trainingDic['NY'].append(wavfile.read(file)) #put the sampling frequency and the song data as a row in the array
+        
+       
     if file.endswith(".wav") and file.startswith("TS"):
         #print(file)
-        trainingDic['TS'][i]=wavfile.read(file) #put tthe sampling frequency and the song data as a row in the array
-        i=i+1  
-    i=0    
+        trainingDic['TS'].append(wavfile.read(file)) #put the sampling frequency and the song data as a row in the array
+          
+       
     if file.endswith(".wav") and file.startswith("FM"):
         #print(file)
-        trainingDic['FM'][i]=wavfile.read(file) #put tthe sampling frequency and the song data as a row in the array
-        i=i+1
-    i=0    
+        trainingDic['FM'].append(wavfile.read(file)) #put the sampling frequency and the song data as a row in the array
+        
+       
     if file.endswith(".wav") and file.startswith("TP"):
         #print(file)
-        trainingDic['TP'][i]=wavfile.read(file) #put tthe sampling frequency and the song data as a row in the array
-        i=i+1             
+        trainingDic['TP'].append(wavfile.read(file)) #put tthe sampling frequency and the song data as a row in the array
+                    
 #and we are done reading the wave
 
 
-print(trainingDic['TS']) #check that the data was stored properly
+print('\n'.join(map(str, trainingDic['TP'][1][1][4800:4809]))) #check that the data was stored properly
 #mk now let's split it into smaller ones
 
 
-"""
-clip=[0][1]
-print(clip)
-print(clip[44100:44200],clip[44100:44200])
-plotly.offline.plot({
-    "data": [Scatter(x=list(range(1,100)),y= clip[44100:44200] )],
-    "layout": Layout(title="hello world")
-})
-#okay now take the fft
-
-
-
-#print('\n'.join([str(pair) for pair in Braver])
-"""
