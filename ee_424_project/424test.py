a0708494bffb8c62cbfc25b194311cdb3c32472a
caldwell <caldwell@iastate.edu>
2018-04-24 21:42:53 -0500

    check for failure of 424test

--- ee_424_project/424test.py
+++ ee_424_project/424test.py
@@ -4,17 +4,29 @@ import os
 from playsound import playsound
 import scipy.io 
 from scipy.io import wavfile
+import scipy.signal
 import youtube_dl
 from subprocess import call
 from numpy import fft
+import numpy as np
 import plotly
 from plotly.graph_objs import Scatter, Layout #this is for plotting maybe unessecary
 import pandas as pd
 import tensorflow as tfs
 #there are 4 artists with 0-19 songs each
 
+def create_spectrogram(song):
 
-print("hello world")
+    rate, sig = wavfile.read(song)
+
+    wav0, wav1 = np.split(sig, 2, axis=1)
+
+    wav_mono = .5*(wav0+wav1)
+    #wav_mono = wav0
+    f, t, Sxx = scipy.signal.spectrogram(np.squeeze(wav_mono), rate)
+
+    return f, t, Sxx
+
 trainingDic={"NY":[],"TS":[],"FM":[],"TP":[]}
 
 #location = "/mnt/c/Users/Valery/424/" #location of where the file is, the music needs to be in the same directory
@@ -25,28 +37,32 @@ location  = "/home/christopher/Music/music_424"
 for file in os.listdir(location):  
     
     if file.endswith(".wav") and file.startswith("NY"):
-        #print(file)
-        trainingDic['NY'].append(wavfile.read(file)) #put the sampling frequency and the song data as a row in the array
+        print(file)
+        create_spectrogram(location+'/'+file)
+        trainingDic['NY'].append(wavfile.read(location+'/'+file)) #put the sampling frequency and the song data as a row in the array
         
        
     if file.endswith(".wav") and file.startswith("TS"):
-        #print(file)
-        trainingDic['TS'].append(wavfile.read(file)) #put the sampling frequency and the song data as a row in the array
+        print(file)
+        create_spectrogram(location+'/'+file)
+        trainingDic['TS'].append(wavfile.read(location+'/'+file)) #put the sampling frequency and the song data as a row in the array
           
        
     if file.endswith(".wav") and file.startswith("FM"):
-        #print(file)
-        trainingDic['FM'].append(wavfile.read(file)) #put the sampling frequency and the song data as a row in the array
+        print(file)
+        create_spectrogram(location+'/'+file)
+        trainingDic['FM'].append(wavfile.read(location+'/'+file)) #put the sampling frequency and the song data as a row in the array
         
        
     if file.endswith(".wav") and file.startswith("TP"):
-        #print(file)
-        trainingDic['TP'].append(wavfile.read(file)) #put tthe sampling frequency and the song data as a row in the array
+        print(file)
+        create_spectrogram(location+'/'+file)
+        trainingDic['TP'].append(wavfile.read(location+'/'+file)) #put tthe sampling frequency and the song data as a row in the array
                     
 #and we are done reading the wave
 
 
-print('\n'.join(map(str, trainingDic['TP'][1][1][4800:4809]))) #check that the data was stored properly
+#print('\n'.join(map(str, trainingDic['TP'][1][1][4800:4809]))) #check that the data was stored properly
 #mk now let's split it into smaller ones
 
 
